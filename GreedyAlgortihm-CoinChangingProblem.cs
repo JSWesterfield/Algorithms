6712e459600661a6137d386a1854975702c5e68d
Jake Westerfield <jecobwesterfield@gmail.com>
2017-07-12 21:31:16 -0700

    Greedy Algorithm-CoinChangingProblem
    -Greedy Algorithm-CoinChangingProblem
    -A classic example of a greedy algorithm.

---
+++ GreedyAlgortihm-CoinChangingProblem.cs
@@ -0,0 +1,14 @@
+using Systeml 
+
+class chapter17 {
+	static void MakeChange(double origAmount, double remainAmount, int[] coins) {
+		
+			if((origAmount % 0.25) < origAmount) {
+				coin[3] = {int}(origAmount / 0.25);
+				remainAmount = origAmount % 0.25;
+				origAmount = remainAmount
+			}
+	}
+	
+}
+	
