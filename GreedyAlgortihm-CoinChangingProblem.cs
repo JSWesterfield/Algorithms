3819f452daafcc95748dcaec59b687daf2ac5038
Jake Westerfield <jecobwesterfield@gmail.com>
2017-07-12 21:46:17 -0700

    additional if statements added
    -additional if statements for the greedy algo.

--- GreedyAlgortihm-CoinChangingProblem.cs
+++ GreedyAlgortihm-CoinChangingProblem.cs
@@ -3,20 +3,31 @@ using Systeml
 class chapter17 {
 	static void MakeChange(double origAmount, double remainAmount, int[] coins) {
 		
-			if((origAmount % 0.25) < origAmount) {
-				coin[3] = {int}(origAmount / 0.25);
-				remainAmount = origAmount % 0.25;
-				origAmount = remainAmount;
-			}
-			
-			if((origAmount % 0.1) < origAmount) {
-				coins[2] = (int)(origAmount / 0.1);
-				remainAmount = origAmount % 0.1;
-				origAmount = remainAmount;
-			}
-			
+		if((origAmount % 0.25) < origAmount) {
+			coin[3] = {int}(origAmount / 0.25);
+			remainAmount = origAmount % 0.25;
+			origAmount = remainAmount;
+		}
 		
+		if((origAmount % 0.1) < origAmount) {
+			coins[2] = (int)(origAmount / 0.1);
+			remainAmount = origAmount % 0.1;
+			origAmount = remainAmount;
+		}
+		
+		if((origAmount % 0.05) < origAmount) {
+			coins[1] = (int)(origAmount / 0.05);
+			remainAmount = origAmount % 0.05;
+			origAmount = remainAmount;
+		}
+		
+		if((origAmount % 0.1) < origAmount){
+			coins[0] = (int)(origAmount / 0.01);
+			remainAmount = origAmount % 0.01;
+		}
 	}
+	
+	
 	
 }
 	
